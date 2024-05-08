6cd208ab8b90fcf7a82dd77dda69c36002bda086
Lucas F. Guanabara <lucasff.guanabara@gmail.com>
2024-05-08 19:55:55 -0300

    add Delete

--- src/route.ts
+++ src/route.ts
@@ -1,6 +1,7 @@
 import {FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply} from "fastify"
 import { CreateCustomerController } from './controllers/CreateCustomerController'
 import { ListCustomersController } from "./controllers/ListCustomerController"
+import { DeleteCustomerController } from './controllers/DeleteCustomerController'
 
 export async function route(fastify:FastifyInstance, options: FastifyPluginOptions) {
 
@@ -14,5 +15,9 @@ export async function route(fastify:FastifyInstance, options: FastifyPluginOptio
 
     fastify.get("/customers", async(request: FastifyRequest, reply: FastifyReply) => {
         return new ListCustomersController().handle(request, reply)
+    })
+
+    fastify.delete("/customer", async(request: FastifyRequest, reply: FastifyReply) => {
+        return new DeleteCustomerController().handle(request, reply)
     })
 }
